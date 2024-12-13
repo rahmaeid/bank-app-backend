463aa51b9a7d766dfdb7c7d752b3903719307b90
Rahma Eid <rahmaeid326@gmail.com>
2024-12-13 11:37:04 +0200

    add branches relations

--- src/booking/branch.service.ts
+++ src/booking/branch.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { access_token_payload } from '../auth/auth.enum';
 import { Branch } from './entities/branch.entity';
+import { classToPlain } from 'class-transformer';
 
 @Injectable()
 export class BranchService {
@@ -12,8 +13,12 @@ export class BranchService {
   ) {}
 
   async getAllBranches({ sub }: access_token_payload) {
-    return this.branchRepo.find({
+    const branches = await this.branchRepo.find({
       relations: ['city', 'country'],
     });
+
+    const plainUser = classToPlain(branches);
+
+    return plainUser;
   }
 }
